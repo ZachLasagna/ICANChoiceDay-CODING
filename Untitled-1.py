223a57a63d63809e252479bcf840219a3a840399
ZachLasagna <160941393+ZachLasagna@users.noreply.github.com>
2024-02-29 15:51:39 -0500

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,26 @@
+#Calculator
+# x = float(input("Enter first number:"))
+# v = input("Enter -, +, *, or /:")
+# y = float(input("Enter second number:"))
+# if y == 0:
+#     print("second number cannot be zero")
+
+# if v == "-":
+#     z = (x-y)
+# elif v == "+":
+#     z = (x+y)
+# elif v == "*":
+#     z = (x*y)
+# elif v == "/":
+#     z = (x/y)
+# else:
+#     print("Error")
+# print (float(z))
+#The other thing
+# x = float(input("Enter first number:"))
+# v = float(input("Enter second number"))
+# z = x % v
+# if z == 0:
+#     print("The first number can be evenly divided by the second number")
+# else:
+#     print("The first number cannot be evenly divided by the second number")
